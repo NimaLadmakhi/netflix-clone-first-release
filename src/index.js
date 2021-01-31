4251575096da752c120e825b0ffad99411d43000
nima ladmakhi <cody18v@gmail.com>
2021-01-31 02:49:11 -0800

    add strcuture of project + mobile navbar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+/** @format */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-ReactDOM.render(<App/>, document.getElementById("root"));
+import Startup from './startup';
+const root = document.getElementById('root');
+ReactDOM.render(<Startup />, root);
